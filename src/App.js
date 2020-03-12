cc298fc8398de3052c0b8093f93ce798cad70708
Olga <hijazin.olga@gmail.com>
2020-03-12 10:37:25 -0800

    Step 3 done

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
   };
   unselectAuthor = () => {
     this.setState({ currentAuthor: null });
+  };
   render() {
     return (
       <div id="app" className="container-fluid">
